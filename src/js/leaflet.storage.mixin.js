d7f240a649d558c752991c3c0336feee0ebf7b83
Шиниев Павел <shpawel@mail.ru>
2017-02-27 11:01:48 +0300

    include to map property "editLayer" change defaultDataLayer, enableEdit, disableEdit

---
+++ src/js/leaflet.storage.mixin.js
@@ -0,0 +1,89 @@
+L.Storage.Map.include({
+
+    editedLayer: null,
+
+    editLayer: function (onSelectCallback) {
+        var container = L.DomUtil.create('div');
+
+        var builder = new L.S.FormBuilder(this, ['datalayers'], {callback: function (e) {
+            this.edit(e);}
+        });  // removeLayer step will close the edit panel, let's reopen it
+        container.appendChild(builder.build());
+
+        var editLink = L.DomUtil.create('a', 'button storage-edit', container);
+        editLink.href = '#';
+        editLink.innerHTML = L._('Edit');
+        L.DomEvent.on(editLink, 'click', function (e) {
+            L.DomEvent.stop(e);
+                this.ui.closePanel();
+                if (!this.editedLayer) {this.editedLayer = builder.helpers['datalayers'].toJS()}
+                onSelectCallback();
+        }, this);
+
+        this.ui.openPanel({data: {html: container}, className: 'dark'})
+    }
+});
+
+L.Storage.Map.addInitHook(function(){
+    var editedLayer = null;
+        that = this;
+    try {
+        Object.defineProperty(this, 'editedLayer', {
+            get: function () {
+                return editedLayer;
+            },
+            set: function (datalayer) {
+                if (editedLayer && editedLayer !== datalayer) {
+                    editedLayer.clear();
+                }
+                editedLayer = datalayer;
+                that.fire('seteditedlayer');
+            }
+        });
+    }
+    catch (e) {
+        // Certainly IE8, which has a limited version of defineProperty
+    }
+});
+
+L.Storage.Map.prototype.defaultDataLayer = function(){
+    if (this.editedLayer) return this.editedLayer;
+
+    var datalayer, fallback;
+    datalayer = this.lastUsedDataLayer;
+    if (datalayer && (!datalayer.isRemoteLayer() || (datalayer.isRemoteLayer() && datalayer.isWFSTLayer())) && datalayer.canBrowse() && datalayer.isVisible()) {
+        return datalayer;
+    }
+    datalayer = this.findDataLayer(function (datalayer) {
+        if ((!datalayer.isRemoteLayer() || (datalayer.isRemoteLayer() && datalayer.isWFSTLayer())) && datalayer.canBrowse()) {
+            fallback = datalayer;
+            if (datalayer.isVisible()) return true;
+        }
+    });
+    if (datalayer) return datalayer;
+    if (fallback) {
+        // No datalayer visible, let's force one
+        this.addLayer(fallback.layer);
+        return fallback;
+    }
+    return this.createDataLayer();
+};
+
+L.Storage.Map.prototype.enableEdit = function() {
+    var that = this;
+    this.editLayer(function() {
+        if (!that.editedLayer) return;
+        L.DomUtil.addClass(document.body, 'storage-edit-enabled');
+        that.editEnabled = true;
+        that.fire('edit:enabled');
+    })
+};
+
+L.Storage.Map.prototype.disableEdit = function() {
+    if (this.isDirty) return;
+    L.DomUtil.removeClass(document.body, 'storage-edit-enabled');
+    this.editedFeature = null;
+    this.editedLayer = null;
+    this.editEnabled = false;
+    this.fire('edit:disabled');
+};
